94b1e6bb1c78a549a0783f31d0b61579efaf625d
Afridi Azmain <Azmain951>
2022-05-23 13:30:05 +0600

    api created to store order data and update stock quantity after placing order, show order by user email api created

--- index.js
+++ index.js
@@ -1,11 +1,67 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nfjhw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    await client.connect();
+    const productCollection = client.db('manufacturer_website').collection('products');
+    const orderCollection = client.db('manufacturer_website').collection('orders');
+    try {
+        app.get('/products', async (req, res) => {
+            const products = await productCollection.find().toArray();
+            res.send(products);
+        });
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        });
+
+        app.patch('/orders', async (req, res) => {
+            const order = req.body;
+            const quantity = order.quantity;
+            const stock = order.stock;
+            const newStock = stock - quantity;
+            const id = order.productId;
+            const query = { _id: ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    stock: newStock,
+                }
+            }
+            const updatedProduct = await productCollection.updateOne(query, updatedDoc);
+            console.log(updatedProduct);
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        });
+
+        app.get('/orders/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const orders = await orderCollection.find(query).toArray();
+            console.log(orders);
+            res.send(orders);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('Hello from Manufacturer!')
