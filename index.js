410622bf585a669c84e0ee5c3bf0b22de274f841
Afridi Azmain <Azmain951>
2022-05-24 17:04:32 +0600

    payment system implemented

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -28,12 +29,37 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-    await client.connect();
-    const productCollection = client.db('manufacturer_website').collection('products');
-    const orderCollection = client.db('manufacturer_website').collection('orders');
-    const userCollection = client.db('manufacturer_website').collection('users');
-    const reviewCollection = client.db('manufacturer_website').collection('reviews');
     try {
+        await client.connect();
+        const productCollection = client.db('manufacturer_website').collection('products');
+        const orderCollection = client.db('manufacturer_website').collection('orders');
+        const userCollection = client.db('manufacturer_website').collection('users');
+        const reviewCollection = client.db('manufacturer_website').collection('reviews');
+        const paymentCollection = client.db('manufacturer_website').collection('payments');
+
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                next();
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden Access' });
+            }
+        }
+
+        app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+            const order = req.body;
+            const price = order.totalPrice;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret })
+        });
+
         app.get('/products', async (req, res) => {
             const products = await productCollection.find().toArray();
             res.send(products);
@@ -46,11 +72,18 @@ async function run() {
             res.send(product);
         });
 
-        app.post('/products', verifyJWT, async (req, res) => {
+        app.post('/products', verifyJWT, verifyAdmin, async (req, res) => {
             const product = req.body;
             const result = await productCollection.insertOne(product);
             res.send(result);
         });
+
+        app.delete('/products/:id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
 
         app.get('/orders', verifyJWT, async (req, res) => {
             const orders = await orderCollection.find().toArray();
@@ -61,11 +94,19 @@ async function run() {
             const email = req.params.email;
             const query = { email };
             const orders = await orderCollection.find(query).toArray();
-            console.log(orders);
             res.send(orders);
         });
 
-        app.patch('/orders', verifyJWT, async (req, res) => {
+        app.get('/order/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('id', id);
+            const query = { _id: ObjectId(id) };
+            const order = await orderCollection.findOne(query);
+            console.log(order)
+            res.send(order);
+        })
+
+        app.patch('/orders', async (req, res) => {
             const order = req.body;
             const quantity = order.quantity;
             const stock = order.stock;
@@ -82,6 +123,22 @@ async function run() {
             const result = await orderCollection.insertOne(order);
             res.send(result);
         });
+
+        app.patch('/orders/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            const query = { _id: ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId
+                }
+            }
+            const updatedOrder = await orderCollection.updateOne(query, updatedDoc);
+            const result = await paymentCollection.insertOne(payment);
+            res.send(updatedOrder);
+
+        })
 
 
         app.get('/user', verifyJWT, async (req, res) => {
@@ -106,12 +163,30 @@ async function run() {
             res.send({ result, token });
         });
 
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email });
+            const isAdmin = user?.role === 'admin';
+            console.log(isAdmin)
+            res.send({ admin: isAdmin });
+        });
+
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params.email;
+            const filter = { email };
+            const updateDoc = {
+                $set: { role: 'admin' }
+            }
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
+
         app.get('/review', async (req, res) => {
             const users = await reviewCollection.find().toArray();
             res.send(users);
         });
 
-        app.post('/review', verifyJWT, async (req, res) => {
+        app.post('/review', async (req, res) => {
             const review = req.body;
             const result = await reviewCollection.insertOne(review);
             res.send(result);
